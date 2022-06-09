2347abbcb804cb54a748c7a8c17a1c02e7dd6fa9
doyun0887@gmail.com <doyun>
2022-06-09 16:59:58 +0900

    초기세팅

---
+++ HomeToGather/HomeToGather/HomeToGatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeToGatherApp.swift
+//  HomeToGather
+//
+//  Created by Doyun Park on 2022/06/09.
+//
+
+import SwiftUI
+
+@main
+struct HomeToGatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
