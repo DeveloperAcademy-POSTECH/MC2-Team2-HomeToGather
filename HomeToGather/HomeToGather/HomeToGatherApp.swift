4751ede783c14e3ca706801ad9cbb209f61bfad6
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-06-10 13:57:52 +0900

    Apple Login 구현

--- HomeToGather/HomeToGather/HomeToGatherApp.swift
+++ HomeToGather/HomeToGather/HomeToGatherApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct HomeToGatherApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
