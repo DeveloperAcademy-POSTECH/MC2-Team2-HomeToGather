d6dbe2d136e150daa637bfa5f78a43eaf137d265
jayden000106 <cars0106@icloud.com>
2022-06-11 18:02:19 +0900

    :sparkles: #10, #11 초대된 파티 리스트 뷰와 내용을 보여주는 뷰 추가

---
+++ HomeToGather/HomeToGather/View/TicketList/Invited/InvitedDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  InvitedDetailView.swift
+//  HomeToGather
+//
+//  Created by 정지혁 on 2022/06/11.
+//
+
+import SwiftUI
+
+struct InvitedDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InvitedDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        InvitedDetailView()
+    }
+}
---
+++ HomeToGather/HomeToGather/View/TicketList/Invited/InvitedListView.swift
@@ -0,0 +1,20 @@
+//
+//  InvitedListView.swift
+//  HomeToGather
+//
+//  Created by 정지혁 on 2022/06/11.
+//
+
+import SwiftUI
+
+struct InvitedListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InvitedListView_Previews: PreviewProvider {
+    static var previews: some View {
+        InvitedListView()
+    }
+}
