d1099acfd5c0d72a0aadd1e215eeac95d8a37b67
jayden000106 <cars0106@icloud.com>
2022-06-12 02:13:41 +0900

    ✨: #14, #15 주최한 초대장 리스트 뷰와 내용을 보여주기 위한 뷰 추가

---
+++ HomeToGather/HomeToGather/View/TicketList/Hosted/HostedDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  HostedDetailView.swift
+//  HomeToGather
+//
+//  Created by 정지혁 on 2022/06/12.
+//
+
+import SwiftUI
+
+struct HostedDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HostedDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        HostedDetailView()
+    }
+}
---
+++ HomeToGather/HomeToGather/View/TicketList/Hosted/HostedListView.swift
@@ -0,0 +1,20 @@
+//
+//  HostedListView.swift
+//  HomeToGather
+//
+//  Created by 정지혁 on 2022/06/12.
+//
+
+import SwiftUI
+
+struct HostedListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HostedListView_Previews: PreviewProvider {
+    static var previews: some View {
+        HostedListView()
+    }
+}
