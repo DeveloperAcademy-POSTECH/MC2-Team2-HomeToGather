020a5231573f0ee2e1b0a3b19c0ec2d5ee80f774
jayden000106 <cars0106@icloud.com>
2022-06-12 17:18:57 +0900

    ✨: #15 파티 규칙, 메뉴와 피드백을 보여주기 위한 카드뷰 생성

---
+++ HomeToGather/HomeToGather/View/TicketList/Hosted/FeedbackCardView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedbackCardView.swift
+//  HomeToGather
+//
+//  Created by 정지혁 on 2022/06/12.
+//
+
+import SwiftUI
+
+struct FeedbackCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedbackCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedbackCardView()
+    }
+}
