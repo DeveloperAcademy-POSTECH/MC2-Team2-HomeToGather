61115012012c7bc9e7c250d9db5dc496402df1c2
jayden000106 <cars0106@icloud.com>
2022-06-13 02:12:46 +0900

    ✨: #18 불참/참여 버튼 구현
    - InvitationView의 상단은 InvitedDetailView를 재사용
    - 하단에 불참, 참여 버튼을 배치

--- HomeToGather/HomeToGather/View/TicketList/InvitationView.swift
+++ HomeToGather/HomeToGather/View/TicketList/InvitationView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct InvitationView: View {
+    var invitationData: Invitation
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            InvitedDetailView(invitationData: invitationData)
+            
+            HStack(spacing: 0) {
+                Button(action: {}, label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 4)
+                            .fill(Color.cardBackgroundColor)
+                            .frame(height: 60)
+                        
+                        Text("불참")
+                            .foregroundColor(.white)
+                            .font(.system(size: 17, weight: .bold))
+                    }
+                })
+                .padding(.trailing, 5)
+                
+                Button(action: {}, label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 4)
+                            .fill(Color.brandColor)
+                            .frame(height: 60)
+                        
+                        Text("참여")
+                            .foregroundColor(.white)
+                            .font(.system(size: 17, weight: .bold))
+                    }
+                })
+                .padding(.leading, 5)
+            }
+            .padding(20)
+        }
     }
 }
 
 struct InvitationView_Previews: PreviewProvider {
     static var previews: some View {
-        InvitationView()
+        InvitationView(invitationData: data[0])
     }
 }
