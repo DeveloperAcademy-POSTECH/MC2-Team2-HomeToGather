3b9f159f690920274fe9ba15fb3a0197194755b2
jayden000106 <cars0106@icloud.com>
2022-06-13 15:10:44 +0900

    🐛: 중복된 네비게이션 뷰 수정
    - ContentView와 MainView에 중복된 네비게이션 뷰 수정

--- HomeToGather/HomeToGather/View/Main/MainView.swift
+++ HomeToGather/HomeToGather/View/Main/MainView.swift
@@ -16,73 +16,72 @@ struct MainView: View {
     }
     
     var body: some View {
-        
-        NavigationView {
-            ZStack {
-                Color.backgroundColor
-                    .ignoresSafeArea()
-                VStack {
-                    
-                    if isTouchedTicket {
-                        VStack {
-                            expanedTicketView(flag: $isTouchedTicket)
-                                .offset(y: -50)
-                                .background(Color.white)
-                                .toolbar {
-                                    ToolbarItem(placement: .navigationBarLeading) {
-                                        Button {
-                                            withAnimation(.easeInOut(duration: 1.2)) {
-                                                isTouchedTicket.toggle()
-                                            }
-                                        } label: {
-                                            Image(systemName: "xmark")
-                                                .foregroundColor(.white)
+        ZStack {
+            Color.backgroundColor
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                if isTouchedTicket {
+                    VStack {
+                        expanedTicketView(flag: $isTouchedTicket)
+                            .offset(y: -50)
+                            .background(Color.white)
+                            .toolbar {
+                                ToolbarItem(placement: .navigationBarLeading) {
+                                    Button {
+                                        withAnimation(.easeInOut(duration: 1.2)) {
+                                            isTouchedTicket.toggle()
                                         }
+                                    } label: {
+                                        Image(systemName: "xmark")
+                                            .foregroundColor(.white)
                                     }
                                 }
+                            }
+                        
+                    }
+                    
+                } else {
+                    ticketView(isTicketGesture: isTicketGesture)
+                        .offset(y: -100)
+                        .onTapGesture {
+                            withAnimation(.easeInOut(duration: 1.2)) {
+                                isTouchedTicket.toggle()
+                            }
+                            
+                        }
+                    
+                    HStack(alignment: .center, spacing: 10, content: {
+                        
+                        NavigationLink {
+                            InvitedListView()
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 4)
+                                    .fill(Color.backgroundColor)
+                                    .frame(width: 170, height: 60)
+                                
+                                Text("초대된 파티")
+                                    .font(.notoSans(withStyle: .Bold, size: 17))
+                                    .foregroundColor(.white)
+                            }
                         }
                         
-                    } else {
-                        ticketView(isTicketGesture: isTicketGesture)
-                            .offset(y: -100)
-                            .onTapGesture {
-                                withAnimation(.easeInOut(duration: 1.2)) {
-                                    isTouchedTicket.toggle()
-                                }
+                        NavigationLink {
+                            HostedListView()
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 4)
+                                    .fill(Color.backgroundColor)
+                                    .frame(width: 170, height: 60)
                                 
+                                Text("주최한 파티")
+                                    .font(.notoSans(withStyle: .Bold, size: 17))
+                                    .foregroundColor(.white)
                             }
-                        
-                        HStack(alignment: .center, spacing: 10, content: {
-                            
-                            NavigationLink {
-                                InvitedListView()
-                            } label: {
-                                ZStack {
-                                    RoundedRectangle(cornerRadius: 4)
-                                        .fill(Color.backgroundColor)
-                                        .frame(width: 170, height: 60)
-                                    
-                                    Text("초대된 파티")
-                                        .font(.notoSans(withStyle: .Bold, size: 17))
-                                        .foregroundColor(.white)
-                                }
-                            }
-                            
-                            NavigationLink {
-                                HostedListView()
-                            } label: {
-                                ZStack {
-                                    RoundedRectangle(cornerRadius: 4)
-                                        .fill(Color.backgroundColor)
-                                        .frame(width: 170, height: 60)
-                                    
-                                    Text("주최한 파티")
-                                        .font(.notoSans(withStyle: .Bold, size: 17))
-                                        .foregroundColor(.white)
-                                }
-                            }
-                        }).offset(y: -15)
-                    }
+                        }
+                    }).offset(y: -15)
                 }
             }
             .preferredColorScheme(.dark)
