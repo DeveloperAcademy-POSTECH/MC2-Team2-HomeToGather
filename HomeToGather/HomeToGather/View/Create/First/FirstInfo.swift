4cf5129c2897b5e21a1f646e23b4aa6ad053013c
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2022-06-14 15:23:29 +0900

    💄 : 디테일 디자인 수정

--- HomeToGather/HomeToGather/View/Create/First/FirstInfo.swift
+++ HomeToGather/HomeToGather/View/Create/First/FirstInfo.swift
@@ -13,8 +13,8 @@ struct FirstInfo: View {
     //    @ObservedObject var partyData = PartyData
     @EnvironmentObject var partyData: PartyData
     
+    @State private var date = Date()
     @State var partyName = ""
-    
     @State var detailAddress = ""
     
     let textLimit = 20
@@ -25,12 +25,13 @@ struct FirstInfo: View {
     var body: some View {
         VStack(alignment: .leading) {
             TitleRow(text: "홈파티 소개")
-                .padding(EdgeInsets(top: 65, leading: 20, bottom: 50, trailing: 36))
+                .padding(EdgeInsets(top: 65, leading: 20, bottom: 36, trailing: 20))
+            
             VStack(alignment: .leading){
                 SubTitleRow(text: "파티 이름")
                 TextField("ex) 파티명을 적어주세요.", text: $partyData.title)
                     .foregroundColor(.white)
-                    .padding(EdgeInsets(top: 0, leading: 6, bottom: 7, trailing: 6))
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 7, trailing: 20))
                     .onReceive(Just($partyName)) { _ in limitText(textLimit) }
                 Rectangle()
                     .fill(color)
@@ -41,10 +42,12 @@ struct FirstInfo: View {
                     Text("\(partyName.count)/20")
                         .foregroundColor(.white)
                         .font(.notoSans(withStyle: .Light, size: 10))
-                }.padding(EdgeInsets(top: 0, leading: 0, bottom: 26, trailing: 0))
+                }
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0))
+                
                 SubTitleRow(text: "소개")
                 ZStack(alignment: .leading){
-                    if partyData.description == "" {
+                    if partyData.description.isEmpty {
                         Text("파티를 소개해보세요.")
                             .font(.notoSans(withStyle: .Light, size: 16))
                             .foregroundColor(.gray)
@@ -54,37 +57,40 @@ struct FirstInfo: View {
                     TextEditor(text: $partyData.description)
                         .padding()
                         .foregroundColor(.white)
-//                        .background(Color.cardBackgroundColor)
+                    //                        .background(Color.cardBackgroundColor)
                         .background(Color.white)
                         .font(.notoSans(withStyle: .Light, size: 16))
                         .lineSpacing(3) //줄 간격
                         .frame(width: 309, height: 85)
-//                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 200)
-//                        .border(color, width: 1)
+                    //                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 200)
+                    //                        .border(color, width: 1)
                     
                 }
             }
-            .padding(20)
+            .padding(EdgeInsets(top: 0, leading: 20, bottom: 30, trailing: 20))
+            
             VStack(alignment: .leading){
                 SubTitleRow(text: "날짜/시간")
-                DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/
-                           , label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
+                DatePicker(selection: $date, label: {Image(systemName: "calendar")})
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 30, trailing: 0))
+                    .accentColor(Color.partyPurple)
+                
                 SubTitleRow(text: "장소")
                 SearchAddressButton()
                 TextField("ex) 상세주소를 입력해주세요.", text: $detailAddress)
                     .foregroundColor(.white)
-                    .padding(EdgeInsets(top: 0, leading: 6, bottom: 0, trailing: 6))
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 7, trailing: 20))
                 Rectangle()
                     .fill(color)
                     .frame(height: width)
                     .edgesIgnoringSafeArea(.horizontal)
             }
-            .padding(20)
+            .padding(EdgeInsets(top: 0, leading: 20, bottom: 53, trailing: 20))
         }
         .frame(width: size)
         .background(Color.cardBackgroundColor)
         .cornerRadius(4)
-//        .padding(EdgeInsets(top: 65, leading: 20, bottom: 50, trailing: 20))
+        //        .padding(EdgeInsets(top: 65, leading: 20, bottom: 50, trailing: 20))
     }
     func limitText(_ upper: Int) {
         if partyName.count > upper {
